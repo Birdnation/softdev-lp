3e2491cea6e4ff0aa5db6a048611ff52a1cf7d55
Rodrigo Dominguez <rodrigo.dominguez@alumnos.ucn.cl>
2021-04-02 22:58:57 -0300

    services hero

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    var bar = new ldBar('.ldBar')
+    var valor = 0;
+    for (let i = 0; i < 101; i++) {
+        bar.set(i);
+        valor = i;
+    }
+    $(".main").hide();
+    $('.loading').delay(2000).fadeOut(500);
+    $('.main').delay(2500).fadeIn(2000);
+    $('.carousel').carousel({
+        interval: 2000
+    })
+})
